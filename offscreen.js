48016d34a750e3f9e27a37101f0b8451ec867d81
weizman <weizmangal@gmail.com>
2024-02-08 20:05:38 +0200

    poc

---
+++ offscreen.js
@@ -0,0 +1,11 @@
+console.log(111,
+    self,
+    window === top,
+    opener,
+    localStorage,
+    sessionStorage,
+    document,
+    document?.cookie,
+);
+
+document.write('<iframe src="https://example.com"></iframe>')
